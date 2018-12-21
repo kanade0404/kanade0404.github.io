206f2d29f41327a38783b6cc3781c65b79283de8
kanade <melty0404@gmail.com>
2018-12-21 15:53:19 +0900

    vue

---
+++ src/components/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <h4>name: kanade0404</h4>
+    <h4>Language</h4>
+    <ul>
+      <li><p>Python</p></li>
+      <li><p>JavaScript</p></li>
+      <li><p>Java</p></li>
+      <li><p>C#</p></li>
+      <li><p>TypeScript</p></li>
+      <li><p>PHP</p></li>
+    </ul>
+    <h4>Framework</h4>
+    <ul>
+      <li><p>Django</p></li>
+      <li><p>.NET Framework</p></li>
+      <li><p>Angular</p></li>
+      <li><p>Nuxt.js</p></li>
+      <li><p>Spring Boot</p></li>
+      <li><p>Laravel</p></li>
+      <li><p>Tensorflow</p></li>
+      <li><p>PyTorch</p></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style scoped>
+li {
+  display: inline-flex;
+}
+</style>
