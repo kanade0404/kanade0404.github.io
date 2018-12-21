4c24de62333f37a4ac5592b311de4231c6b43ca4
kanade <melty0404@gmail.com>
2018-12-21 23:39:05 +0900

    とりあえずメイン画面

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app light>
+    <section id="main">
+      <div class="content">
+        <div class="content-item icon">
+          <img src="https://avatars3.githubusercontent.com/u/32828502?s=400&u=334422e5e252dbe944aa8b3ab56d2f1d6ca4398c&v=4" alt="">
+        </div>
+        <div class="content-item name">かなで(kanade)</div>
+        <div class="content-item job">Web Engineer</div>
+        <div class="content-item location">Kanagawa Japan</div>
+      </div>
+    </section>
+  </v-app>
+</template>
+
+<style scoped>
+#app{
+  padding: 30px;
+}
+#main {
+  margin: 0 auto;
+  background-color: #efefef;
+  width: 60%;
+}
+img{
+  border-radius: 50%;
+  height: 200px;
+  border: 6px solid #008080;
+}
+.content{
+  text-align: center;
+}
+.content-item{
+  font-size: 30px;
+  margin: 10px;
+  color:#008000;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title>kanade page</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>About Me</v-btn>
+      <v-btn flat>Skills</v-btn>
+      <v-btn flat>Contact</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
